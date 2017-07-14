500a87c9815e9c4a63d38c5541f3abe7fdb59fbb
ctsygiel <ctsygiel@gmail.com>
2017-07-14 11:31:20 -1000

    one bookshelf component, one book component

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../App.css'
+import Book from './Book'
+
+
+class Bookshelf extends Component {
+  render() {
+    const { books, title } = this.props
+      return (
+        <div className='bookshelf'>
+          <h2 className='bookshelf-title'>{title}</h2>
+            <div className='bookshelf-books'>
+              <ol id='currentlyReading' className='books-grid'>
+                {books.map((book) => (
+                  <Book
+                    book={book}
+                  />
+                ))}
+              </ol>
+            </div>
+        </div>
+       )
+    }
+}
+
+export default Bookshelf
